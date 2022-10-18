7d1470320d8bf4eb01b86f66c159d9f43a0226cb
cihuyyama <miqbalalhabib@gmail.com>
2022-10-18 14:30:33 +0700

    first commit

---
+++ src/main/java/tratix/learnmigratedb/LearnmigratedbApplication.java
@@ -0,0 +1,13 @@
+package tratix.learnmigratedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnmigratedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnmigratedbApplication.class, args);
+	}
+
+}
